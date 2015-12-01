c5bbb32e7f99683a35e8891df3afc10b30616d6c
Nik Tripp <ntripp13@RM117-ST09.mcse.georgefox.edu>
2015-12-01 14:11:24 -0800

    Generated stubs for Game
    Generated stubs for Game. Incidentally, figured out Eclipse shortcuts
    and increased stub output exponentially.

--- Game.java
+++ Game.java
@@ -10,7 +10,65 @@ import Field.java
  */
 public class Game
 {
-
+   private Field _theField;
+   
+   public Game()
+   {
+      // TODO
+      System.out.println("Game() NYD");
+   }
+   
+   public static Field getField()
+   {
+      // TODO
+      System.out.println("getField() NYD");
+      
+      return null;
+   }
+   
+   public boolean didRevealBomb(Field f)
+   {
+      // TODO
+      System.out.println("didRevealBomb(f) NYD");
+      
+      return false;
+   }
+   
+   public void blowUp()
+   {
+      // TODO
+      System.out.println("blowUp() NYD");
+   }
+   
+   public void winner()
+   {
+      // TODO
+      System.out.println("winner() NYD");
+   }
+   
+   public int getYRowsMove()
+   {
+      // TODO
+      System.out.println("getYRowsMove() NYD");
+      
+      return -1;
+   }
+   
+   public int getXColumnsMove()
+   {
+      // TODO
+      System.out.println("getXColumnsMove() NYD");
+      
+      return -1;
+   }
+   
+   public String toString()
+   {
+      // TODO
+      System.out.println("toString() NYD");
+      
+      return null;
+   }
 }
 
 public static void main(String[] args)
