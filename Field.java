c1b83e7799bca45caa31d9d287fa565af6102008
Siyang Luo <sluo14@georgefox.edu>
2015-12-01 22:28:44 -0800

    Basically finished the most part of the Field class, but don’t know how to implement the revealTile(Tile t) and not sure if toString() was implemented correctly…

--- Field.java
+++ Field.java
@@ -1,4 +1,6 @@
-import Tile.java
+import java.util.Random;
+
+//import Tile.java
 
 /**
  * @author Michael
@@ -15,39 +17,153 @@ public class Field
    private int _xColumns;
    private Tile[][] _field;
    
-   public Field(int size)
-   {
-      System.out.println("Field(int size) NYD");
-   }
    
    public Field(int size)
    {
-      System.out.println("Field(int size, int minePercentage) NYD");
+      this(size, size);
    }
+   
    
    public Field(int yRows, int xColumns)
    {
-      System.out.println("Field(int yRows, int xColumns) NYD");
+      this(yRows, xColumns, 30);
    }
+   
    
    public Field(int yRows, int xColumns, int minePercentage)
    {
-      System.out.println("Field(int yRows, int xColumns, int minePercentage) "
-            + "NYD");
+      _yRows = yRows;
+      _xColumns = xColumns;
+      _field = new Tile[_yRows][_xColumns];
+      Random random = new Random();
+      boolean isBomb = false;
+      
+      for (int i = 0; i < _yRows; i++)
+      {
+         for (int j = 0; j < _xColumns; i++)
+         {
+            if (random.nextInt(99) <= minePercentage)
+            {
+               isBomb = true;
+            }
+            
+            _field[i][j] = new Tile(i, j, isBomb);
+         }
+      }
    }
+   
    
    public Tile getTileAt(int yRow, int xColumn)
    {
-      System.out.println("getTileAt(int yRow, int xColumn) NYD");
-      
-      return null;
+      return _field[yRow][xColumn];
    }
+   
    
    public Tile[] getNeighbors(Tile t)
    {
-      System.out.println("getNeighbors(Tile t) NYD");
-      return null;
+      Tile[] neighbors = null;
+      
+      // If the Tile t is at the corner of the field, it only has 3 neighbors.
+      if (t.getX() == 0 && t.getY() == 0 
+            || t.getX() == _xColumns - 1 && t.getY() == 0 
+            || t.getX() == _xColumns - 1 && t.getY() == _yRows - 1 
+            || t.getX() == 0 && t.getY() == _yRows - 1)
+      {
+         neighbors = new Tile[3];
+         
+         if (t.getX() == 0 && t.getY() == 0)
+         {
+            neighbors[0] = getTileAt(0,1); 
+            neighbors[1] = getTileAt(1,1); 
+            neighbors[2] = getTileAt(1,0); 
+         } // if
+         
+         if (t.getX() == _xColumns - 1 && t.getY() == 0)
+         {
+            neighbors[0] = getTileAt(1,t.getX() - 1); 
+            neighbors[1] = getTileAt(1,t.getX() - 2); 
+            neighbors[2] = getTileAt(0,t.getX() - 1); 
+         } // if
+         
+         if (t.getX() == _xColumns - 1 && t.getY() == _yRows - 1)
+         {
+            neighbors[0] = getTileAt(t.getY()-1, t.getX() - 2); 
+            neighbors[1] = getTileAt(t.getY()-2, t.getX() - 2); 
+            neighbors[2] = getTileAt(t.getY()-2, t.getX() - 1); 
+         } // if
+         
+         if (t.getX() == 0 && t.getY() == _yRows - 1)
+         {
+            neighbors[0] = getTileAt(t.getY()-2, 0); 
+            neighbors[1] = getTileAt(t.getY()-2, 1); 
+            neighbors[2] = getTileAt(t.getY()-1, 1); 
+         } // if
+      } // if
+      
+      
+      // If the Tile t is at the edge of the field, it has 5 neighbors. 
+      else if (t.getY() == 0 
+            || t.getX() == _xColumns - 1 
+            || t.getY() == _yRows - 1 
+            || t.getX() == 0)
+      {
+         neighbors = new Tile[5];
+         
+         if (t.getY() == 0)
+         {
+            neighbors[0] = getTileAt(0, t.getX()+1); //_field[_xColumns + 1][0];
+            neighbors[1] = getTileAt(1, t.getX()+1); //_field[1][1];
+            neighbors[2] = getTileAt(1, t.getX()); //_field[0][1];
+            neighbors[3] = getTileAt(1, t.getX()-1); //_field[1][0];
+            neighbors[4] = getTileAt(0, t.getX()-1); //_field[1][1];
+         }
+         
+         if (t.getX() == _xColumns - 1)
+         {
+            neighbors[0] = getTileAt(t.getY()-1,t.getX()-1);
+            neighbors[1] = getTileAt(t.getY()-1, t.getX());
+            neighbors[2] = getTileAt(t.getY()+1, t.getX());
+            neighbors[3] = getTileAt(t.getY()+1, t.getX()-1);
+            neighbors[4] = getTileAt(t.getY(), t.getX()-1);
+         }
+         
+         if (t.getY() == _yRows - 1)
+         {
+            neighbors[0] = getTileAt(t.getY()-1, t.getX()-1);
+            neighbors[1] = getTileAt(t.getY()-1, t.getX());
+            neighbors[2] = getTileAt(t.getY()-1,t.getX()+1);
+            neighbors[3] = getTileAt(t.getY(),t.getX()+1);
+            neighbors[4] = getTileAt(t.getY(),t.getX()-1);
+         }
+         
+         if (t.getX() == 0)
+         {
+            neighbors[0] = getTileAt(t.getY()-1, 0);
+            neighbors[1] = getTileAt(t.getY()-1, 1);
+            neighbors[2] = getTileAt(t.getY(), 1);
+            neighbors[3] = getTileAt(t.getY()+1, 1);
+            neighbors[4] = getTileAt(t.getY()+1,0);
+         } // if
+      } // else if
+      
+      // Otherwise the Tile t should have 8 neighbors. 
+      else
+      {
+         neighbors = new Tile[8];
+         
+         neighbors[0] = getTileAt(t.getY()-1, t.getX()-1);
+         neighbors[1] = getTileAt(t.getY()-1, t.getX());
+         neighbors[2] = getTileAt(t.getY()-1, t.getX()+1);
+         neighbors[3] = getTileAt(t.getY(), t.getX()+1);
+         neighbors[4] = getTileAt(t.getY()+1, t.getX()+1);
+         neighbors[5] = getTileAt(t.getY()+1, t.getX());
+         neighbors[6] = getTileAt(t.getY()+1, t.getX()-1);
+         neighbors[7] = getTileAt(t.getY(), t.getX()-1);
+      } // else
+      
+      return neighbors;
    }
+   
    
    /**
     * Returns the number of "rows" in the field
@@ -69,14 +185,41 @@ public class Field
    
    public boolean isWinner()
    {
-      System.out.println("isWinner() NYD");
-      return false;
+      boolean win = true;
+      
+      for (int i = 0; i < _yRows; i++)
+      {
+         for (int j = 0; j < _xColumns; i++)
+         {
+            if ( (_field[i][j].isBomb() == true 
+                  && _field[i][j].isRevealed() == true) 
+                  || (_field[i][j].isBomb() == false 
+                  && _field[i][j].isRevealed() == false) )
+            {
+               win = false;
+            }
+         }
+      }
+      
+      return win;
    }
+   
    
    public void revealTile(Tile t)
    {
-      System.out.println("revealTile(Tile t) NYD");
+      if (! t.isRevealed())
+      {
+         if (t.isBomb())
+         {
+            System.out.println("You hit a bomb. You lost. ");
+         }
+         else
+         {
+            
+         }
+      }
    }
+   
    
    /**
     * Returns a String representing the field in the form
